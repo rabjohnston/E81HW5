6c4bc89f410951c4a1d42ee1f191535c737618da
Rob Johnston <me@me.com>
2016-11-08 06:59:20 +0800

    Add the texts/ directory. Make all text lowercase. Add Processing file (WIP)

---
+++ processing.py
@@ -0,0 +1,25 @@
+from nltk.stem.porter import *
+import nltk
+import string
+
+class Processing:
+    """
+    http://www.cs.duke.edu/courses/spring14/compsci290/assignments/lab02.html 
+    """
+    def stem_tokens(tokens, stemmer=PorterStemmer()):
+        stemmed = []
+        for item in tokens:
+            stemmed.append(stemmer.stem(item))
+        return stemmed
+
+
+    def get_tokens(text):
+        """
+        Remove the punctuation using the character deletion step of translate
+
+        text: lowercase text
+        :return: list of tokens
+        """
+        no_punctuation = text.translate(None, string.punctuation)
+        tokens = nltk.word_tokenize(no_punctuation)
+        return tokens
